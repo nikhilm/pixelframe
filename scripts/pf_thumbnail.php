cf7451d7d507b4d2bf76c76df701289d77c353dc
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-02-22 12:38:38 +0000

    Added php script to generate thumbnails. Empty function, no implementation yet
    git-svn-id: svn://localhost/home/nikhil/repository/pixelframe/trunk@81 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

---
+++ scripts/pf_thumbnail.php
@@ -0,0 +1,35 @@
+<? /*
+  pf_thumbnail.php - contains functions related to generating thumbnails
+*
+* Copyright (C)      2007  Nikhil Marathe <nsm.nikhil@gmail.com>
+*               
+* This file is part of the PixelFrame project.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/**
+* Creates a thumbnail from an image.
+
+ @param string $image Filename of the original image(png or jpeg)
+ @param string $thumbnail File to write the thumbnail to.
+ @param integer $width Width of the thumbnail
+ @param integer $height Hieght of the thumbnail
+
+ @return bool true on success, false on failure
+*/
+function makeThumbnail($image, $thumbnail, $width, $height)
+{
+} 
