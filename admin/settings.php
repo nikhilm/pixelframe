62195d09b871b22cba5f6bd9446135a9a3db22de
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-02-23 15:32:33 +0000

    Added clean version
    git-svn-id: svn://localhost/home/nikhil/repository/pixelframe/trunk@91 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

---
+++ admin/settings.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<title>Pixelframe: Settings</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+
+<div id="wrapper">
+<div id="header">
+<img src="../logo.png" />
+</div>
+</div>
+
+</body>
+</html>
