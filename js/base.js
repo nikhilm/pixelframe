5f84676fbace9b3104e75aa6fdc252aca0311ec2
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-02-24 13:12:41 +0000

    Added some more features from JUICE to base.js
    git-svn-id: svn://localhost/home/nikhil/repository/pixelframe/trunk@102 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

--- js/base.js
+++ js/base.js
@@ -1,4 +1,16 @@
 /**
+* Adds a method to easily add properties to an objects prototype.
+* To extend Class with properties of AnotherClass just do
+* <code>Object.extend(Class.prototype, AnotherClass)</code>
+*/
+Object.extend = function(dest, src) {
+//alert("Copying properties of " + src + " to " + dest);
+for(property in src)
+dest[property] = src[property];
+return dest;
+}
+
+/**
 * Converts an array type object to an array.
 * Eg. arguments array
 * @param {Object} The array like object
@@ -10,6 +22,41 @@ for(var i = 0; i < iterable.length; i++)
 ret.push(iterable[i]);
 return ret;
 }
+
+_ArrayExtension = {
+/**
+* Calls a specified function for each element in the array.
+* The function is passed the current array element as the first argument.
+* Example:
+* <code>[1, 2, 3, 4, 5].each(function(num) { sqr = num * num; });</code>
+* @param {Function} The function to call for each element.
+*/
+each:function(func) {
+for(var i = 0; i < this.length;i++)
+func(this[i]);
+},
+    
+/**
+* @returns the last array element
+*/
+last:function() { return this[this.length - 1]; },
+    
+/**
+* Checks if a value is in the array.
+* Returns the position if found, otherwise -1.
+* <code>[1, 2, 3].indexOf(2) -> 1</code>
+* @param {Object} The object to check.
+* @returns the index if found or -1.
+*/
+indexOf: function(object) {
+for(var i = 0; i < this.length; i++)
+if(object == this[i]) return i;
+return -1;
+}
+    
+}
+
+Object.extend(Array.prototype, _ArrayExtension);
 
 /**
 * Returns all elements which have the class cName.
