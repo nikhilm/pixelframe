7914c451bed8ff27b5988cd69e3141fd94d97e53
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-03-15 13:25:33 +0000

    Added mock settings-control.php
    git-svn-id: svn://localhost/home/nikhil/repository/pixelframe/trunk@179 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

---
+++ admin/settings-control.php
@@ -0,0 +1,30 @@
+<?/*
+  settings-control.php - handles and delegates modification of pixelframe settings.
+*
+* Copyright (C)      2007  Nikhil Marathe <nsm.nikhil@gmail.com>
+*               
+* This file is part of the PixelFrame project.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+header('Content-Type: text/xml');
+print('<?xml version="1.0" encoding="utf-8" ?>');
+print('<reply>');
+print('<status>success</status> ');
+print('<message>Successfully calculated the answer to life the universe and everything</message>');
+print('</reply>');
+
+?>
