d4359322db5a7ad22a1d234f4a8ea1f11b1bef22
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-03-18 16:49:41 +0000

    Added delete album script
    git-svn-id: svn://localhost/home/nikhil/repository/pixelframe/trunk@211 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

---
+++ scripts/pf_delete_album.php
@@ -0,0 +1,85 @@
+<?php
+/*
+*  pf_delete_album.php - Handles deletion of albums.
+*
+* Copyright (C)      2007  Nikhil Marathe <nsm.nikhil@gmail.com>
+*               
+* This file is part of the PixelFrame project.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+include_once("pf_constants.php");
+include_once(PF_SCRIPTS_DIR."pf_configparser.php");
+
+// success and error are external functions in ../admin/settings-control.php
+
+function init($args) {
+    $name = $args['name'];
+    
+    $cp = new ConfigReader(PF_CONFIG_FILE);
+    $albums = $cp->getChildren("settings/albums");
+    if(!$albums) {
+        error("It seems you don't have any albums. Why don't you create one.");
+        return;
+    }
+    $location = "";
+    //find our album location
+    foreach($albums as $album) {
+        if($album['attributes']['name'] == $name) {
+            $location = $album['attributes']['location'];
+        }
+    }
+    
+    if($location == "") {
+        error("Could not find album $name");
+        return;
+    }
+    
+    //see if it exists
+    if(!is_dir($_SERVER['DOCUMENT_ROOT'].'/'.$location)) {
+        error("Album directory does not exist");
+        return;
+    }
+    
+    $thumbnailDir = $_SERVER['DOCUMENT_ROOT'].'/'.$location.'/'.PF_THUMBNAIL_DIR;
+    //try removing the thumbnails
+    chdir($thumbnailDir);
+    $pattern = array("*.jpg", "*.jpeg", "*.png");
+    foreach($pattern as $pat) {
+        foreach(glob($pat) as $file) {
+            if(!unlink($file)) {
+                error("Could not remove thumbnail for image $file");
+                return;
+            }
+        }
+    }
+    
+    //try removing thumbnails directory
+    if(!rmdir($thumbnailDir)) {
+        error("Could not remove thumbnails directory for album $name in $location");
+        return;
+    }
+    
+    //remove from config
+    $cp = new ConfigWriter(PF_CONFIG_FILE);
+    if(!($cp->removeWithAttributes("settings/albums/album", "name", $name) && $cp->close())) {
+        error("Could not remove album $name");
+        return;
+    }
+    
+    success("Successfully removed album $name");
+}
+?>
