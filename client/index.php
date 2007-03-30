b0777422e5a805a7afcaed4075c67ab735fb1e01
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-03-30 11:56:43 +0000

    Added album checking code
    git-svn-id: svn://localhost/home/nikhil/repository/pixelframe/trunk@276 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

---
+++ client/index.php
@@ -0,0 +1,58 @@
+<?php
+/*
+*  index.php - Generates album for viewing by loading theme and stuff
+*
+* Copyright (C)      2007  Nikhil Marathe <nsm.nikhil@gmail.com>
+*               
+* This file is part of the PixelFrame project.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+//accepts GET parameter album=<album name>
+
+include_once("../scripts/pf_constants.php");
+include_once(PF_SCRIPTS_DIR."pf_configparser.php");
+
+//begin new session
+session_start();
+
+//check if album exists
+$albumName = $_GET['album'];
+
+$albumLocation = "";
+$albumTheme = "";
+
+$cp = new ConfigReader(PF_CONFIG_FILE);
+$albums = $cp->getChildren("settings/albums");
+if($albums) {
+    $albumExists = FALSE;
+    foreach($albums as $album) {
+        if($album['attributes']['name'] == $albumName) {
+            $albumLocation = $album['attributes']['location'];
+            $albumTheme = $album['attributes']['theme'];
+            $albumExists = TRUE;
+        }
+    }
+    if(!albumExists) {
+        print("There is no album called $albumName.");
+        return;
+    }
+}
+else {
+    print("There are no albums");
+    return;
+}
+
