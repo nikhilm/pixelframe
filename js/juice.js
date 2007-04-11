ff97f81740bd44fb6ee74895f852ec9be79b8316
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-04-11 11:16:40 +0000

    Removed typo
    git-svn-id: svn://localhost/home/nikhil/repository/pixelframe/trunk@354 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

--- js/juice.js
+++ js/juice.js
@@ -192,7 +192,7 @@ return {x:_27,y:top,width:w,height:h};
 },addEvent:function(_2d,_2e,_2f){
 if(window.addEventListener){
 this.addEventListener(_2d,_2e,_2f);
-}else{this.id);
+}else{
 this.attachEvent("on"+_2d,_2e);
 }
 },removeEvent:function(_30,_31,_32){
